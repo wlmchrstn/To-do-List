9ebcdc3c4daf5515f7f249e4774941d8e3c5a09f
William Christian <wlmchrstn@gmai.com>
2019-09-09 09:53:28 +0700

    [Will] env var

--- server.js
+++ server.js
@@ -4,24 +4,23 @@ const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDoc = require('./swagger.json');
-// const env = process.env.NODE_ENV //|| 'development'
+const env = process.env.NODE_ENV || 'development'
 const router = require('./routes/index.js');
 
 /* istanbul ignore else */
-// if (
-//     //env == 'development' || 
-//     env == 'test') {
-//     require('dotenv').config();
-// }
+if (
+    env == 'development' || env == 'test') {
+    require('dotenv').config();
+}
 
-// const dbConfig = {
-//     development: process.env.DBDEV,  
-//     test: process.env.DBTEST,
-//     user: process.env.DBLOGIN,
-//     production: process.env.DBPROD,
-// }
+const dbConfig = {
+    development: process.env.DBDEV,  
+    test: process.env.DBTEST,
+    user: process.env.DBLOGIN,
+    production: process.env.DBPROD,
+}
 
-mongoose.connect("mongodb+srv://William:mirai2704@todoapi-tmbsb.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true})
+mongoose.connect(dbConfig[env], { useNewUrlParser: true, useCreateIndex: true})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
